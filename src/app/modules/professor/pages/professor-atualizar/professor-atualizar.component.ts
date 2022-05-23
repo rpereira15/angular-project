163b1a208ddcc6c09a0044bda370e5fbe2f90cff
Rômulo Pereira <romulo.stads@hotmail.com>
2022-05-23 20:29:58 -0300

    Atualização 23/05/2022

--- src/app/modules/professor/pages/professor-atualizar/professor-atualizar.component.ts
+++ src/app/modules/professor/pages/professor-atualizar/professor-atualizar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProfessorDetalhado } from '../../models/professor.model';
+import { ProfessorService } from '../../services/professor.service';
 
 @Component({
   selector: 'app-professor-atualizar',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessorAtualizarComponent implements OnInit {
 
-  constructor() { }
+  professor = {
+    
+  } as ProfessorDetalhado;
+
+  constructor(private activedRoute: ActivatedRoute,
+    private professorService: ProfessorService,
+    private routeNavigate: Router) {
+      
+     }
 
   ngOnInit(): void {
+    
+    const id = this.activedRoute.snapshot.params['id'];
+    this.carregaProfessor(id); 
+  }
+
+  carregaProfessor(id: number) {
+    this.professorService.buscarUmProfessorService(id)
+    .then(result => {
+      this.professor = !!result ? result : {} as ProfessorDetalhado;
+    })
+  }
+
+  salvarController() {
+    this.professorService.atualizarService(this.professor)
+    .then(() => {
+      this.routeNavigate.navigate(['professor/list']);
+    })
+
   }
 
 }
